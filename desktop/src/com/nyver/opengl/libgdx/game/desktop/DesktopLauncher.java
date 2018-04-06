71fc18070a167df5b0f663ada8b79cc81b759997
Iurii.Novitskii <Iurii.Novitskii@Netcracker.com>
2018-04-06 16:25:53 +0300

    Initial commit

---
+++ desktop/src/com/nyver/opengl/libgdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nyver.opengl.libgdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nyver.opengl.libgdx.game.LibGDXGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGDXGame(), config);
+	}
+}
